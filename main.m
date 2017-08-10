96c5c9ee8ec9cbfde81566fc630d043279defd55
Simon Graham <simon_graham@hotmail.co.uk>
2017-08-10 15:53:29 +0100

    changed to cater for csv

--- main.m
+++ main.m
@@ -9,65 +9,63 @@ close all
 % University of Warwick, UK.
 %------------------------------------------------------------------
 
-file_ext = '.png';
+file_ext = '.csv';
 groundtruth_files = dir(['/ground_truth_path/*',file_ext]);
 groundtruth_path = '/ground_truth_path/';
 prediction_path = '/prediction_path/';
 
-% Note: Image names in prediction and ground truth file must be the same
+% Note: File names in prediction and ground truth must be the same
 % Change above paths accordingly and file 
-% Images must be in binary format. If not use im2bw function 
 %------------------------------------------------------------------
 
-stats_measure = 'jaccard';
+stats_measure = 'quad_weighted_kappa';
 
-if strcmp(stats_measure,'jaccard') == 1
-    jac = 0;
-    stats = {jac};
-elseif strcmp(stats_measure,'dice') == 1
-    dice = 0;
-    stats = {dice};
+if strcmp(stats_measure,'quad_weighted_kappa') == 1
+    quad_kappa = 0;
+    stats = {quad_kappa};
+elseif strcmp(stats_measure,'hamming_loss') == 1
+    hamming = 0;
+    stats = {hamming};
 elseif strcmp(stats_measure,'accuracy') == 1
     acc = 0;
     stats = {acc};
-elseif strcmp(stats_measure,'tp_fp_fn') == 1
-    tp = 0;
-    fp = 0;
-    fn = 0;
-    stats = {tp,fp,fn};
+elseif strcmp(stats_measure,'mean_consequential_error') == 1
+    mce = 0;
+    stats = {mce};
+elseif strcmp(stats_measure,'log_loss') == 1
+    log_loss = 0;
+    stats = {log_loss};
+elseif strcmp(stats_measure,'matthews_correlation_coeff') == 1
+    mcc = 0;
+    stats = {mcc};
 end
 %----------------------------------------------------------------
     
 for i = 1:length(groundtruth_files)
-    image_name = groundtruth_files(i).name;
-    disp( sprintf( 'Processing Image: %s', image_name ));
-    pred_path = [prediction_path,image_name];
-    prediction = imread(pred_path);
-    gt_path = [groundtruth_path,image_name];
-    gt = imread(gt_path);
-    prediction = imresize(prediction, [size(gt,1) size(gt,2)]);
+    file_name = groundtruth_files(i).name;
+    disp( sprintf( 'Processing File: %s', file_name ));
+    pred_path = [prediction_path,file_name];
+    gt_path = [groundtruth_path,file_name];
     
-    if strcmp(stats_measure,'jaccard') == 1
-        jac_ = JaccardCoefficient(gt,prediction);
-        stats{1} = jac_ + stats{1};
-    elseif strcmp(stats_measure,'dice') == 1
-        dice_ = DiceIndex(gt,prediction);
-        stats{1} = dice_ + stats{1};
+    if strcmp(stats_measure,'quad_weighted_kappa') == 1
+        quad_kappa = QuadraticWeightedKappa(gt,prediction);
+        stats = {quad_kappa};
+    elseif strcmp(stats_measure,'hamming_loss') == 1
+        hamming_ = HammingLoss(gt,prediction);
+        stats{1} = hamming_ + stats{1};
     elseif strcmp(stats_measure,'accuracy') == 1
         acc_ = Accuracy(gt,prediction);
         stats{1} = acc_ + stats{1};
-    elseif strcmp(stats_measure,'tp_fp_fn') == 1
-        [tp_,fp_,fn_] = find_tp_fp_fn(prediction,gt);
-        stats{1} = tp_ + stats{1};
-        stats{2} = fp_ + stats{2};
-        stats{3} = fn_ + stats{3};
+    elseif strcmp(stats_measure,'mean_consequential_error') == 1
+        mce_ = MeanConsequentialError(gt,prediction);
+        stats{1} = mce_ + stats{1};
+    elseif strcmp(stats_measure,'log_loss') == 1
+        logloss_ = LogLoss(gt,prediction);
+        stats{1} = logloss_ + stats{1};
+    elseif strcmp(stats_measure,'matthews_correlation_coeff') == 1
+        mcc_ = MatthewsCorrelationCoefficient(gt,prediction);
+        stats{1} = mcc_ + stats{1};
     end
-end
 
 stats
-
-if strcmp(stats_measure,'tp_fp_fn') == 1
-    [f1,pr,re] = Calculate_F1Score(stats{1},stats{2},stats{3});
-    disp( sprintf( 'F1: %d, Precision: %d, Recall: %d', f1,pr,re ));
-end
 
