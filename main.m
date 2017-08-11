ed482e8a8a78da4e7774b9658c524a1e2611097b
Simon Graham <simon_graham@hotmail.co.uk>
2017-08-11 12:14:29 +0100

    main

--- main.m
+++ main.m
@@ -10,12 +10,12 @@ close all
 %------------------------------------------------------------------
 
 file_ext = '.csv';
-groundtruth_files = dir(['/ground_truth_path/*',file_ext]);
-groundtruth_path = '/ground_truth_path/';
-prediction_path = '/prediction_path/';
+groundtruth_file = '/Users/simongraham/Desktop/test_gt_results.csv';
+csv_split = strsplit(groundtruth_file,'/');
+csv_name = csv_split(end)
+csv_file = dlmread(groundtruth_file);
 
-% Note: File names in prediction and ground truth must be the same
-% Change above paths accordingly and file 
+% Change above paths accordingly
 %------------------------------------------------------------------
 
 stats_measure = 'quad_weighted_kappa';
@@ -41,31 +41,27 @@ elseif strcmp(stats_measure,'matthews_correlation_coeff') == 1
 end
 %----------------------------------------------------------------
     
-for i = 1:length(groundtruth_files)
-    file_name = groundtruth_files(i).name;
-    disp( sprintf( 'Processing File: %s', file_name ));
-    pred_path = [prediction_path,file_name];
-    gt_path = [groundtruth_path,file_name];
-    
-    if strcmp(stats_measure,'quad_weighted_kappa') == 1
-        quad_kappa = QuadraticWeightedKappa(gt,prediction);
-        stats = {quad_kappa};
-    elseif strcmp(stats_measure,'hamming_loss') == 1
-        hamming_ = HammingLoss(gt,prediction);
-        stats{1} = hamming_ + stats{1};
-    elseif strcmp(stats_measure,'accuracy') == 1
-        acc_ = Accuracy(gt,prediction);
-        stats{1} = acc_ + stats{1};
-    elseif strcmp(stats_measure,'mean_consequential_error') == 1
-        mce_ = MeanConsequentialError(gt,prediction);
-        stats{1} = mce_ + stats{1};
-    elseif strcmp(stats_measure,'log_loss') == 1
-        logloss_ = LogLoss(gt,prediction);
-        stats{1} = logloss_ + stats{1};
-    elseif strcmp(stats_measure,'matthews_correlation_coeff') == 1
-        mcc_ = MatthewsCorrelationCoefficient(gt,prediction);
-        stats{1} = mcc_ + stats{1};
-    end
+disp( sprintf( 'Processing File: %s', csv_name{1}));
+
+if strcmp(stats_measure,'quad_weighted_kappa') == 1
+    quad_kappa = QuadraticWeightedKappa(csv_file(:,1),csv_file(:,2),0,1);
+    stats = {quad_kappa};
+elseif strcmp(stats_measure,'hamming_loss') == 1
+    hamming_ = HammingLoss(csv_file);
+    stats{1} = hamming_ + stats{1};
+elseif strcmp(stats_measure,'accuracy') == 1
+    acc_ = Accuracy(csv_file);
+    stats{1} = acc_ + stats{1};
+elseif strcmp(stats_measure,'mean_consequential_error') == 1
+    mce_ = MeanConsequentialError(csv_file);
+    stats{1} = mce_ + stats{1};
+elseif strcmp(stats_measure,'log_loss') == 1
+    logloss_ = LogLoss(csv_file);
+    stats{1} = logloss_ + stats{1};
+elseif strcmp(stats_measure,'matthews_correlation_coeff') == 1
+    mcc_ = MatthewsCorrelationCoefficient(csv_file);
+    stats{1} = mcc_ + stats{1};
+end
 
 stats
 
