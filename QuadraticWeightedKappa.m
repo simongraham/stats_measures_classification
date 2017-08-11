3c1ba7bb9ac020d100881152bf14c9040038c1c9
Simon Graham <simon_graham@hotmail.co.uk>
2017-08-11 11:49:49 +0100

    separated confusion function

--- QuadraticWeightedKappa.m
+++ QuadraticWeightedKappa.m
@@ -1,4 +1,4 @@
-function [score] = quadraticWeightedKappa(ground_truth, predicted, minRating, maxRating)
+function [kappa] = quadraticWeightedKappa(ground_truth, predicted, minRating, maxRating)
 
 % Author: Simon Graham
 % Tissue Image Analytics Lab
@@ -18,26 +18,11 @@ if nargin==3
 else
     M = confusionMatrix(csv_file);
 end
+
 [dx,dy] = meshgrid(1:size(M,1),1:size(M,2));
 d = (dx-dy).^2 / range(dx(:)).^2;
 E = sum(M,2)*sum(M,1);E = E / sum(M(:));
-score = 1 -(sum(d(:).*M(:))/sum(M(:))) / (sum(d(:).*E(:))/sum(E(:)));
 
+kappa = 1 -(sum(d(:).*M(:))/sum(M(:))) / (sum(d(:).*E(:))/sum(E(:)));
 
-function M = confusionMatrix(X, minRating, maxRating)
-%CONFUSIONMATRIX   Calculates the confusion matrix between two raters
-
-if nargin==3
-    u = minRating:maxRating;
-else
-    u = min(X(:)):max(X(:));
-end
-nU = length(u);
-M = zeros(nU);
-
-for i=1:nU
-    for j=1:nU
-        M(i,j) = sum( (X(:,1)==u(i)) + (X(:,2)==u(j)) == 2);
-    end
-end
 
